9765b5992b7be81f439a7ff81be896263a9996b5
TejasPatel <tjp083@gmail.com>
2020-08-30 14:41:34 -0400

    Few Updation

--- app/src/main/java/com/example/goshoes/CreateAccount.java
+++ app/src/main/java/com/example/goshoes/CreateAccount.java
@@ -1,14 +1,18 @@
 package com.example.goshoes;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.provider.MediaStore;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.Gravity;
@@ -30,15 +34,49 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class CreateAccount extends AppCompatActivity {
 
     private EditText user_name,user_email,user_pwd1,user_pwd2;
     private TextView v1,v2,iv1,iv2;
     private Button createbtn;
+    private ImageView imageView;
     String name,email,password,repassword;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+
+    private FirebaseStorage firebaseStorage;
+    private static int PICK_IMAGE = 123;
+    private Uri imagePath;
+    private StorageReference storageReference;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null)
+        {
+
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -49,7 +87,7 @@ public class CreateAccount extends AppCompatActivity {
         progressDialog =new ProgressDialog(CreateAccount.this);
         progressDialog.setMessage("Creating...");
 
-
+        imageView = findViewById(R.id.DpImage);
         user_name = findViewById(R.id.CreateName);
         user_email = findViewById(R.id.CreateEmail);
         user_pwd1 = findViewById(R.id.CreatePassword);
@@ -61,6 +99,10 @@ public class CreateAccount extends AppCompatActivity {
         iv2 = findViewById(R.id.notvisible2);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
 
 
         user_pwd1.setOnTouchListener(new View.OnTouchListener() {
@@ -110,6 +152,24 @@ public class CreateAccount extends AppCompatActivity {
             }
         });
 
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
+                vb.vibrate(20);
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Choose Image"),PICK_IMAGE);
+            }
+        });
+
+
+
         createbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,7 +181,7 @@ public class CreateAccount extends AppCompatActivity {
                 //mp.start();
 
 
-                if (user_name.getText().toString().trim().length()==0 || user_email.getText().toString().trim().length()==0 || user_pwd1.getText().toString().trim().length()==0 || user_pwd2.getText().toString().trim().length()==0)
+                if (user_name.getText().toString().trim().length()==0 || user_email.getText().toString().trim().length()==0 || user_pwd1.getText().toString().trim().length()==0 || user_pwd2.getText().toString().trim().length()==0 || imagePath == null)
                 {
                     Toast toast = Toast.makeText(CreateAccount.this,"Enter All Details",Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
@@ -216,8 +276,31 @@ public class CreateAccount extends AppCompatActivity {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference reference = firebaseDatabase.getReference().child("Users").child(firebaseAuth.getUid());
 
+
         UserProfile userProfile = new UserProfile(email,name);
         reference.setValue(userProfile);
+
+        StorageReference imageReference = storageReference.child("User Profile Images").child(firebaseAuth.getUid());
+        UploadTask uploadTask = imageReference.putFile(imagePath);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+
+                Toast toast = Toast.makeText(CreateAccount.this,"Upload Failed",Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/example/goshoes/ProfileFragmet.java
+++ app/src/main/java/com/example/goshoes/ProfileFragmet.java
@@ -1,15 +1,38 @@
 package com.example.goshoes;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
 public class ProfileFragmet extends Fragment {
+
+    private TextView name,email;
+    private ImageView profileImage;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
 
 
     @Nullable
@@ -17,9 +40,63 @@ public class ProfileFragmet extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-        View event_View =  inflater.inflate(R.layout.fragment_profile,container,false);
+        View view =  inflater.inflate(R.layout.fragment_profile,container,false);
 
-        return event_View;
+        name = view.findViewById(R.id.ProfileName);
+        email = view.findViewById(R.id.ProfileEmail);
+        profileImage = view.findViewById(R.id.ProfileImage);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        DatabaseReference childreference = databaseReference.child("Users").child(firebaseAuth.getUid());
+
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child("User Profile Images").child(firebaseAuth.getUid()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+
+                Picasso.get().load(uri).into(profileImage);
+
+            }
+        });
+
+
+
+        childreference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+
+                if (userProfile != null) {
+                    name.setText(userProfile.getUsername());
+                    email.setText(userProfile.getUseremail());
+                }
+                else
+                {
+                    name.setText("");
+                    email.setText("");
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                //Toast.makeText(Main2Activity.this,databaseError.getCode(),Toast.LENGTH_LONG).show();
+                Toast toast = Toast.makeText(getContext(),databaseError.getCode(),Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        });
+
+
+        return view;
 
     }
 }
