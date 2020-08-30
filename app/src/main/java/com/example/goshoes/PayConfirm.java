c04c2af724ce0f03a65eb4a55d432a3ea3008676
TejasPatel <tjp083@gmail.com>
2020-08-30 14:53:15 -0400

    Payment Confirmation Part is updated with Firebase Database

--- app/src/main/java/com/example/goshoes/PayConfirm.java
+++ app/src/main/java/com/example/goshoes/PayConfirm.java
@@ -1,8 +1,10 @@
 package com.example.goshoes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -11,11 +13,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
 public class PayConfirm extends AppCompatActivity {
 
     private TextView name,price;
     private Button confirmbtn;
     private ImageView confirm_image;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +44,60 @@ public class PayConfirm extends AppCompatActivity {
         confirm_image = findViewById(R.id.confirm_imageview);
         confirmbtn = findViewById(R.id.confirm_button);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        DatabaseReference childreference = databaseReference.child("Cart Details").child(firebaseAuth.getUid());
+
+
         Intent intent = getIntent();
-        final String received_Name =  intent.getStringExtra("shoe_name");
-        final int received_Image = intent.getIntExtra("shoe_image",0);
-        final String received_Price =  intent.getStringExtra("shoe_price");
+        final String s =  intent.getStringExtra("shoe_quantity");
 
-        price.setText(received_Price);
-        name.setText(received_Name);
-        confirm_image.setImageResource(received_Image);
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child("Cart Shoe Image").child(firebaseAuth.getUid()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
 
+            @Override
+            public void onSuccess(Uri uri) {
+
+                Picasso.get().load(uri).into(confirm_image);
+            }
+        });
+
+        childreference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                CartDetails cartDetails = snapshot.getValue(CartDetails.class);
+
+
+                String s1 = cartDetails.getShoeprice();
+                int x = Integer.valueOf(s1.substring(1));
+                int y = Integer.valueOf(s);
+                String mix = String.valueOf(x*y);
+
+
+                if (cartDetails != null) {
+
+                    name.setText(cartDetails.getShoename());
+                    price.setText("$"+mix);
+                }
+                else
+                {
+                    name.setText("");
+                    price.setText("");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast toast = Toast.makeText(PayConfirm.this,"Something Wrong",Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        });
 
 
         confirmbtn.setOnClickListener(new View.OnClickListener() {
@@ -46,9 +108,10 @@ public class PayConfirm extends AppCompatActivity {
                 toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
                 toast.show();
 
-                Intent intent = new Intent(PayConfirm.this,HomePage.class);
+                Intent intent = new Intent(PayConfirm.this, HomePage.class);
                 startActivity(intent);
             }
         });
     }
+
 }
